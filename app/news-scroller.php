8796f86258151f27aaf46d9a94fd51345b922739
LAPTOP-5DQOFJHH\Ram Pandey <ram07icfai@gmail.com>
2018-05-27 19:34:45 +0530

    updated news scroller

--- app/news-scroller.php
+++ app/news-scroller.php
@@ -1,6 +1,8 @@
-<div class='marquee' data-duration='5000' data-gap='10' data-duplicated='true' >
-    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
-    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
-    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
-    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
-</div>
+<section class="news-scroller clearfix" >
+    <div class="col-md-12" >
+        <div class='marquee' data-duration='5000' data-gap='10' data-duplicated='true' >
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit END.</span>
+        </div> 
+    </div>
+</section>
